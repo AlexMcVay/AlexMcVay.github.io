0f31511f9fd802454df5f8160951b90422ce55ff
Alexandra McVay <alya.mcvay@gmail.com>
2025-03-11 19:45:34 -0700

    functional project cards

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 
+/**
+ * Resize an iframe to fit its contents.
+ *
+ * The iframe is resized to the height of its content. This function should be
+ * called whenever the iframe's content changes.
+ *
+ * @param {HTMLIFrameElement} iframe The iframe to be resized.
+ */
 function resizeIframe(iframe) {
   iframe.style.height = "auto"; // Reset height to auto to allow it to shrink
   const newHeight = iframe.contentWindow.document.body.scrollHeight;
@@ -33,6 +41,11 @@ document.addEventListener('DOMContentLoaded', function() {
 +  });
  });
 
+
+/**
+ * Send an email to the portfolio owner with the given name, email, and message.
+ * @function
+ */
  function sendEmail() {
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
@@ -40,4 +53,34 @@ document.addEventListener('DOMContentLoaded', function() {
   var subject = "New message from " + name;
   var mailtoLink = "mailto:alya.mcvay@gmail.com?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(message);
   window.location.href = mailtoLink;
+}
+
+/**
+ * Filters and displays cards based on the specified tag.
+ * 
+ * @param {string} tag - The tag used to filter cards. If 'all', all cards are displayed.
+ * 
+ * It then iterates over the buttons to remove the "active" class from all buttons
+ * and adds the "active" class to the clicked button. For each card, it checks if the 
+ * card's tags include the specified tag or if the tag is "all". If true, the card is 
+ * displayed by removing the "hidden" class; otherwise, the card is hidden by adding 
+ * the "hidden" class.
+ */
+
+
+function filterCards(tag) {
+  let cards = document.querySelectorAll(".card");
+  let buttons = document.querySelectorAll(".filter-buttons button");
+
+  buttons.forEach(btn => btn.classList.remove("active"));
+  event.target.classList.add("active");
+
+  cards.forEach(card => {
+      let cardTags = card.getAttribute("data-tags").split(" ");
+      if (tag === "all" || cardTags.includes(tag)) {
+          card.classList.remove("hidden");
+      } else {
+          card.classList.add("hidden");
+      }
+  });
 }
