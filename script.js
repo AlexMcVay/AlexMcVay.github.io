7d4f2fe4ee5111da2dd798099f536a67032caee9
Alexandra McVay <alya.mcvay@gmail.com>
2025-03-20 19:49:45 -0700

    Merge branch 'main' of https://github.com/AlexMcVay/AlexMcVay.github.io

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
  */
 function resizeIframe(iframe) {
   iframe.style.height = "auto"; // Reset height to allow shrinking
-  const newHeight = iframe.contentWindow.document.body.scrollHeight;
+  const newHeight = 900;
   iframe.style.height = newHeight + "px"; // Set new height
 }
 
@@ -102,3 +102,40 @@ function filterCards(tag, button) {
   });
 }
 
+
+var wordsToType = document.querySelector("span[words]").getAttribute("words").split(','), 
+            typer =  document.querySelector("span[words]"), 
+            typingSpeed = (parseInt(typer.getAttribute('typing-speed')) || 70), 
+            typingDelay = (parseInt(typer.getAttribute('typing-delay')) || 700);
+    
+var currentWordIndex = 0, currentCharacterIndex = 0; 
+
+function type(){
+
+    var wordToType = wordsToType[currentWordIndex%wordsToType.length];
+
+    if(currentCharacterIndex < wordToType.length){
+        typer.innerHTML += wordToType[currentCharacterIndex++];
+        setTimeout(type, typingSpeed);
+    }else{
+
+        setTimeout(erase, typingDelay);
+    }
+
+}
+function erase(){
+    var wordToType = wordsToType[currentWordIndex%wordsToType.length]; 
+    if(currentCharacterIndex >0){
+        typer.innerHTML = wordToType.substr(0, --currentCharacterIndex -1);
+        setTimeout(erase, typingSpeed);
+    }else{
+
+        currentWordIndex++; 
+        setTimeout(type, typingDelay);
+    }
+
+}
+
+window.onload = function(){
+    type(); 
+}
